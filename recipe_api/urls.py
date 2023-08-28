e8d3147003448e9398acf00d54ed22c5fd5f22a9
Ray Luna <leon.luna.ray@gmail.com>
2023-08-28 14:03:57 -0700

    setup base template

--- recipe_api/urls.py
+++ recipe_api/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path("recipes/", include("recipes.urls")),
     path("users/", include("users.urls")),
     path("admin/", admin.site.urls),
