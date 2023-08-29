848a464d724866d592367859f09a7f1858805187
Ray Luna <leon.luna.ray@gmail.com>
2023-08-28 22:04:45 -0700

    add crud operations

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,62 @@
+import json
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, redirect
+from django.views.decorators.csrf import csrf_exempt
+from .models import Recipe
+from .forms import RecipeForm
 
+@csrf_exempt
 def index(request):
     response_data = {"message": "Hello, world. You're at the recipes endpoint."}
     return JsonResponse(response_data)
+
+@csrf_exempt
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    recipe_list = [{'id': recipe.id, 'title': recipe.title} for recipe in recipes]
+    return JsonResponse(recipe_list, safe=False)
+
+@csrf_exempt
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    recipe_data = {
+        'id': recipe.id,
+        'title': recipe.title,
+        'ingredients': recipe.ingredients,
+        'instructions': recipe.instructions,
+        'created_at': recipe.created_at,
+    }
+    return JsonResponse(recipe_data)
+
+@csrf_exempt
+def recipe_create(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        form = RecipeForm(data)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'message': 'Recipe created successfully'})
+        else:
+            return JsonResponse({'error': form.errors}, status=400)
+    return JsonResponse({'error': 'Invalid request method'}, status=405)
+
+@csrf_exempt
+def recipe_update(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    if request.method == 'PUT':
+        data = json.loads(request.body)
+        form = RecipeForm(data, instance=recipe)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'message': 'Recipe updated successfully'})
+        else:
+            return JsonResponse({'error': form.errors}, status=400)
+    return JsonResponse({'error': 'Invalid request method'}, status=405)
+
+@csrf_exempt
+def recipe_delete(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    if request.method == 'DELETE':
+        recipe.delete()
+        return JsonResponse({'message': 'Recipe deleted successfully'})
+    return JsonResponse({'error': 'Invalid request method'}, status=405)
