9d88744e9948a79d926025ab1653e98e52614ded
Ray Luna <leon.luna.ray@gmail.com>
2023-08-29 20:31:05 -0700

    update allowed hosts settings

--- recipe_api/settings.py
+++ recipe_api/settings.py
@@ -8,11 +8,11 @@ from decouple import config, Csv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# ENV
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
-
-ALLOWED_HOSTS = []
-
+print(ALLOWED_HOSTS)
 
 # Application definition
 
