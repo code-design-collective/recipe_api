045934658ccf828943cc35e35a27284c2a41fa8e
Ray Luna <leon.luna.ray@gmail.com>
2023-08-31 21:01:54 -0700

    add template

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 
 from rest_framework import status
@@ -11,17 +11,11 @@ from rest_framework.permissions import IsAuthenticated
 from .serializers import UserSerializer
 
 
-def index(request):
-    return render(request, 'home.html')
-
-
 @api_view(['POST'])
 def login(request):
-    print('LOGIN! 🔑')
     user = get_object_or_404(User, username=request.data['username'])
-    # Todo determine if not found or wrong pw, add status code
     if not user.check_password(request.data['password']):
-        return Response({"detail": "Not found."})
+        return Response({"detail": "Not found."}, status=status.HTTP_400_BAD_REQUEST)
 
     token, created = Token.objects.get_or_create(user=user)
     serialzer = UserSerializer(instance=user)
@@ -31,7 +25,6 @@ def login(request):
 
 @api_view(['POST'])
 def signup(request):
-    print('SIGNUP! 📝')
     serializer = UserSerializer(data=request.data)
 
     if serializer.is_valid():
@@ -43,12 +36,11 @@ def signup(request):
 
         return Response({"token": token.key, "user": serializer.data})
 
-    return Response(serializer.errors)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def test_token(request):
-    print('TEST TOKEN! 🪙')
     return Response(f"passed for {request.user.email}")
--- recipe_api/urls.py
+++ recipe_api/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
 urlpatterns = [
+    path('', views.index),
     path("admin/", admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path("api/recipes/", include("apps.recipes.urls")),
