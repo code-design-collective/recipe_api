2511d7e47535d1c0295d306be26162361ec21b3d
Ray Luna <leon.luna.ray@gmail.com>
2023-08-31 19:56:11 -0700

    setup signup endpoint

--- recipe_api/urls.py
+++ recipe_api/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from . import views
 
 urlpatterns = [
@@ -7,4 +7,7 @@ urlpatterns = [
     path("api/recipes/", include("apps.recipes.urls")),
     path('api-auth/', include('rest_framework.urls')),
     path("admin/", admin.site.urls),
+    re_path('login', views.login),
+    re_path('signup', views.signup),
+    re_path('test_token', views.test_token),
 ]
--- recipe_api/views.py
+++ recipe_api/views.py
@@ -1,4 +1,41 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+
+from .serializers import UserSerializer
+
 
 def index(request):
     return render(request, 'home.html')
+
+
+@api_view(['POST'])
+def login(request):
+    print('LOGIN! 🔑')
+    return Response({})
+
+
+@api_view(['POST'])
+def signup(request):
+    print('SIGNUP! 📝')
+    serializer = UserSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username=request.data['username'])
+        user.set_password(request.data['password'])
+        user.save()
+        token = Token.objects.create(user=user)
+
+        return Response({"token": token.key, "user": serializer.data})
+
+    return Response(serializer.errors)
+
+@api_view(['POST'])
+def test_token(request):
+    print('TEST TOKEN! 🪙')
+    return Response({})
