81287b385239da780be200ccdfc9d5af04e71dd0
Ray Luna <leon.luna.ray@gmail.com>
2023-09-03 10:29:24 -0700

    refactor login/signup views to use email

--- apps/users/views.py
+++ apps/users/views.py
@@ -12,16 +12,22 @@ from rest_framework.permissions import IsAuthenticated
 from .serializers import UserSerializer
 
 
+from django.contrib.auth import authenticate
+
 @api_view(['POST'])
 def login(request):
-    user = get_object_or_404(settings.AUTH_USER_MODEL, username=request.data['username'])
-    if not user.check_password(request.data['password']):
-        return Response({"detail": "Not found."}, status=status.HTTP_400_BAD_REQUEST)
+    email = request.data.get('email')
+    password = request.data.get('password')
 
-    token, created = Token.objects.get_or_create(user=user)
-    serialzer = UserSerializer(instance=user)
+    user = authenticate(request, email=email, password=password)
 
-    return Response({"token": token.key, "user": serialzer.data})
+    if user is not None:
+        token, created = Token.objects.get_or_create(user=user)
+        serializer = UserSerializer(instance=user)
+        return Response({"token": token.key, "user": serializer.data})
+    else:
+        return Response({"detail": "Invalid credentials."}, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 @api_view(['POST'])
@@ -30,14 +36,15 @@ def signup(request):
 
     if serializer.is_valid():
         serializer.save()
-        user = settings.AUTH_USER_MODEL.objects.get(username=request.data['username'])
+        user = serializer.instance
         user.set_password(request.data['password'])
         user.save()
-        token = Token.objects.create(user=user)
+        token, created = Token.objects.get_or_create(user=user)
 
         return Response({"token": token.key, "user": serializer.data})
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 @api_view(['GET'])
