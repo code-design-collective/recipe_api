d3c65f87f6ac08a109b560eb27d3a62c4397400c
Ray Luna <leon.luna.ray@gmail.com>
2023-09-03 09:47:59 -0700

    refactor for users

--- apps/recipes/models.py
+++ apps/recipes/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 class Recipe(models.Model):
     title = models.CharField(max_length=200)
     ingredients = models.TextField()
     instructions = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- apps/recipes/serializers.py
+++ apps/recipes/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from .models import Recipe
 
 class RecipeSerializer(serializers.ModelSerializer):
+    author = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Recipe
-        fields = '__all__'
+        fields = ['id', 'title', 'ingredients', 'instructions', 'created_at', 'author']
+
+    def create(self, validated_data):
+        validated_data['author'] = self.context['request'].user
+        return super().create(validated_data)
